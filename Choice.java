134a1db5ffb4dee6a4f268e3086430a32d015fcb
Avinash <avinashshirole2001@gmail.com>
2023-02-20 20:01:35 +0530

    first commited

---
+++ Choice.java
@@ -0,0 +1,29 @@
+package maccess;
+
+public class Choice {
+	
+	String b;
+	String valid(String s1) {
+		
+		switch(s1) {
+		case "1":
+			    b="1";
+			    break;
+		case "2":
+			b="2";
+			break;
+		case "3":
+			b="3";
+			break;
+		case "4":
+			b="4";
+			break;
+		case "5":
+			b="5";
+			break;
+			
+		}
+		
+		return b;
+	}
+}
